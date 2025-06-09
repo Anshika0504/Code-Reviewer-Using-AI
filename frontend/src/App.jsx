eeafd12ccf023d115dd803c69352842eb654cc21
Anshika Agarwal <anshikaagarwal0405@gmail.com>
2025-06-09 15:48:19 +0530

    error detection completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,20 @@ import "prismjs/themes/prism-tomorrow.css"
 import Editor from "react-simple-code-editor"
 import prism from "prismjs"
 import { useEffect, useState } from 'react'
-
+import axios from 'axios'
 function App() {
   const [code, setCode] = useState(`function sum() {\n  return 1 + 1;\n}`);
+  const [review,setReview]=useState(``)
+
 useEffect(()=>{
     prism.highlightAll();
-  })
+  },[])
+
+  async function reviewCode(){
+  const response=await axios.post('http://localhost:3000/ai/get-review',{code})
+  setReview(response.data)
+  console.log(response.data)
+  } 
 
   return (
     <>
@@ -22,18 +30,21 @@ highlight={code=>prism.highlight(code,prism.languages.javascript,"javascript")}
 padding={10}
 style={{
   fontFamily:'"Fira code","Fira Mono","monospace"',
-  fontSize:12,
+  fontSize:16,
   border:"1px solid #ddd",
   borderRadius:"5px",
   height:"100%",
   width:"100%"
 }}
 ></Editor>
-  <div className="review">Review</div>
+  <div className="review" onClick={reviewCode} >
+    Review</div>
 </div>
       </div>
       <div className="right">
-      
+      {
+        review
+      }
       </div>
      </main>
     </>
